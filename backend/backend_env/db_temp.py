28f2d712c119b86d1bd6e5a1b190dc21bcd1cf31
= <=>
2024-03-07 21:44:16 +1100

    Create STATISTICS, INCIDENCE tables

--- backend/backend_env/db_temp.py
+++ backend/backend_env/db_temp.py
@@ -84,6 +84,31 @@ with conn.cursor() as cur:
     # cur.execute(
     #     """ALTER TABLE SSREMINDER ADD CONSTRAINT USERS_SSREMINDER FOREIGN KEY ( users_id ) REFERENCES  USERS ( users_id );""")
 
+    # # Cancer Statistics Table
+    # cur.execute("""CREATE TABLE IF NOT EXISTS CANCER_STATISTICS (
+    #     cancer_group VARCHAR(100),
+    #     cancer_year INTEGER,
+    #     cancer_gender CHAR(1),
+    #     cancer_age_group VARCHAR(20),
+    #     cancer_incidence_count INTEGER,
+    #     cancer_age_specific_incidence_rate DECIMAL,
+    #     cancer_mortality_count INTEGER,
+    #     cancer_age_specific_mortality_rate DECIMAL);""")
+    
+    # cur.execute("""ALTER TABLE CANCER_STATISTICS ADD CONSTRAINT CANCER_STATISTICS_CHK CHECK (cancer_gender in ('M','F'))""")
+
+    # # Cancer Incidence Table
+    # cur.execute("""CREATE TABLE IF NOT EXISTS CANCER_INCIDENCE (
+    # cancer_group VARCHAR(100),
+    # cancer_year INTEGER,
+    # cancer_gender CHAR(1),
+    # cancer_state VARCHAR(50),
+    # cancer_incidence_count INTEGER);""")
+    
+    # cur.execute("""ALTER TABLE CANCER_INCIDENCE ADD CONSTRAINT CANCER_GENDER_CHK CHECK (cancer_gender in ('M', 'F'))""")
+    # cur.execute("""ALTER TABLE CANCER_INCIDENCE ADD CONSTRAINT CANCER_STATE_CHK CHECK (cancer_state in ('ACT', 'NSW', 'NT', 'QLD', 'SA', 'TAS', 'VIC', 'WA'))""")
+    # cur.execute("""ALTER TABLE CANCER_INCIDENCE ADD CONSTRAINT CANCER_INCIDENCE_UNQ UNIQUE (cancer_group, cancer_year, cancer_gender, cancer_state)""")
+    
     # Insertion into Suburb Table (removed and saved elsewhere, really long)
 
     # Test insertion into Users Table
@@ -103,67 +128,3 @@ with conn.cursor() as cur:
 
     conn.commit()
     conn.close()
-
-# # cur.execute("""CREATE TABLE MORTALITY (
-# #     mortl_id SERIAL PRIMARY KEY,
-# #     mortl_cancer_type VARCHAR(100),
-# #     mortl_year INTEGER,
-# #     mortl_sex CHAR(1),
-# #     mortl_age_group VARCHAR(20),
-# #     mortl_count INTEGER
-# # );""")
-
-
-# # cur.execute(
-# #     "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-# print(cur.execute("DROP TABLE test;"))
-# # Pass data to fill a query placeholders and let Psycopg perform
-# # the correct conversion (no more SQL injections!)
-# # cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-
-# # # Query the database and obtain data as Python objects
-# # cur.execute("SELECT * FROM test;")
-# # print(cur.fetchone())
-
-
-# # Make the changes to the database persistent
-# conn.commit()
-# conn.close()
-
-
-# Connect to your SQLite3 cloud database
-# conn = sqlite3.connect('sqlitecloud://admin:TA11sun360@nv9zosfaik.sqlite.cloud:8860\sun360.db')  # Replace 'path_to_your_database_file.db' with your database file path
-
-# # Create a cursor object to interact with the database
-# cursor = conn.cursor()
-
-# # Define a function to create tables based on your model classes
-# def create_tables():
-#     # Iterate over each model class and create the corresponding table
-#     for model_class in [YourModel1, YourModel2]:  # Add more model classes as needed
-#         table_name = model_class.__tablename__
-#         columns = ', '.join([f'{column_name} {column_type}' for column_name, column_type in model_class.__table__.items()])
-#         create_table_query = f'CREATE TABLE IF NOT EXISTS {table_name} ({columns})'
-#         cursor.execute(create_table_query)
-
-#     # Commit changes and close connection
-#     conn.commit()
-#     conn.close()
-
-# # Call the function to create tables
-# # create_tables()
-
-# conn.close()
-
-
-# up.uses_netloc.append("postgres")
-# url = up.urlparse(os.environ.get("DATABASE_URL")
-#                   or "postgres://emgxnsgc:EnycfxImNzGTrseE9G9P11Gqm2u3L_ZI@rain.db.elephantsql.com/emgxnsgc")
-
-
-# conn = psycopg2.connect(database=url.path[1:],
-#                         user=url.username,
-#                         password=url.password,
-#                         host=url.hostname,
-#                         port=url.port
-#                         )
